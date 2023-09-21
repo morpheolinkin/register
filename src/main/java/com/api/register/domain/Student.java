7bf478b94a385fed0bc8dfc7a3b1d35b3aae82ec
Jefferson M Silva <jeffersonmedeirosdasilva@gmail.com>
2023-09-21 12:08:25 -0300

    Student updated with CPF, RG and birth certification.

--- src/main/java/com/api/register/domain/Student.java
+++ src/main/java/com/api/register/domain/Student.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
-import org.hibernate.Hibernate;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -20,32 +26,45 @@ public class Student implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @NotEmpty
     private String name;
+    @NotEmpty
     private LocalDate age;
+    @NotEmpty
     private String sex;
     private String responsible;
     private String address;
+    @CPF
+    private String cpf;
+    private String rg;
+    private String birth_certificate;
 
     public Student(Integer id, String name, LocalDate age, String sex,
-                   String responsible, String address ) {
+                   String responsible, String address,
+                   String cpf, String rg, String birth_certificate ) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.sex = sex;
         this.responsible = responsible;
         this.address = address;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.birth_certificate = birth_certificate;
     }
 
     @Override
-    public boolean equals(Object o) {
+    public final boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
         Student student = (Student) o;
         return getId() != null && Objects.equals(getId(), student.getId());
     }
-
     @Override
-    public int hashCode() {
-        return getClass().hashCode();
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
